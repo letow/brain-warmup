78dc487e5823c369400b5161796ebc63135485dc
George <71167099+letow@users.noreply.github.com>
2023-10-21 23:14:24 +0300

    moved project from cra to webpack

---
+++ config/build/BuildLoaders.ts
@@ -0,0 +1,45 @@
+import { BuildOptions } from "./types/config";
+import MiniCssExtractPlugin from "mini-css-extract-plugin";
+import webpack from "webpack";
+
+export function buildLoaders({ isDev }: BuildOptions): webpack.RuleSetRule[] {
+    const svgLoader = {
+        test: /\.svg$/,
+        use: ["@svgr/webpack"],
+    };
+
+    const fileLoader = {
+        test: /\.(png|jpe?g|gif|ttf|woff)$/i,
+        use: [
+            {
+                loader: "file-loader",
+            },
+        ],
+    };
+
+    const scssLoader = {
+        test: /\.s[ac]ss$/i,
+        use: [
+            isDev ? "style-loader" : MiniCssExtractPlugin.loader,
+            {
+                loader: "css-loader",
+                options: {
+                    modules: {
+                        auto: Boolean(/\.module\./i),
+                        localIdentName: isDev
+                            ? "[path][name]__[local]--[hash:base64:8]"
+                            : "[hash:base64:8]",
+                    }, // css modules
+                },
+            },
+            "sass-loader",
+        ],
+    };
+    const typescriptLoader = {
+        test: /\.tsx?$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+    };
+
+    return [fileLoader, svgLoader, typescriptLoader, scssLoader];
+}
