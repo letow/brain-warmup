06542af409d9f42700c39e6cb3ec7ba56de363bf
George <71167099+letow@users.noreply.github.com>
2023-10-23 23:22:52 +0300

    added code splitting via react.lazy

--- src/App.tsx
+++ src/App.tsx
@@ -3,19 +3,22 @@ import './App.scss';
 import Kawashima from './pages/Kawashima/Kawashima';
 import MainPage from './pages/MainPage/MainPage';
 import Stroop from './pages/Stroop/Stroop';
+import { Suspense } from 'react';
 
 const App = () => {
     return (
         <div className="App">
-            <Routes>
-                <Route path="/kawashima" element={<Kawashima />} />
+            <Suspense fallback={<div>Loading...</div>}>
+                <Routes>
+                    <Route path="/kawashima" element={<Kawashima />} />
 
-                <Route path="/stroop" element={<Stroop />} />
+                    <Route path="/stroop" element={<Stroop />} />
 
-                <Route path="/" element={<MainPage />} />
+                    <Route path="/" element={<MainPage />} />
 
-                <Route path="*" element={<Navigate to="/" />} />
-            </Routes>
+                    <Route path="*" element={<Navigate to="/" />} />
+                </Routes>
+            </Suspense>
         </div>
     );
 };
