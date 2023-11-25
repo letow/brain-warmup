b979b2dbfa875ca3aa089393e4023ee43c55223b
George <71167099+letow@users.noreply.github.com>
2023-11-25 19:16:39 +0300

    (failed) attempt to create sudoku generator

--- src/pages/Sudoku/Sudoku.tsx
+++ src/pages/Sudoku/Sudoku.tsx
@@ -5,29 +5,47 @@ import InfoBox from '../../components/InfoBox/InfoBox';
 import { shuffle } from '../../helpers/shuffle';
 
 const Sudoku = () => {
-    const numbers = Array.from(Array(9), () => Array.from({ length: 9 }, (_, k) => k + 1));
+    let ok = false;
+    while (!ok) {
+        const numbers = Array.from(Array(9), () => Array.from({ length: 9 }, (_, k) => k + 1));
 
-    const isUnique = (n: number, a: number[]) => {
-        return a.filter(i => i === n).length === 1;
-    };
+        const isArrLegit = (arr: number[]) => {
+            return arr.length === new Set(arr).size;
+        };
 
-    numbers.forEach(arr => shuffle(arr));
+        numbers.forEach(arr => shuffle(arr));
 
-    for (let i = 0; i < 9; i++) {
-        const colNums = [];
-        for (let j = 0; j < 9; j++) {
-            colNums.push(numbers[j][i]);
+        const colNumbers = [];
+        for (let i = 0; i < 9; i++) {
+            const colNums = [];
+            for (let j = 0; j < 9; j++) {
+                colNums.push(numbers[j][i]);
+            }
+            colNumbers.push(colNums);
         }
 
-        console.log(colNums);
+        console.log('another');
 
-        for (let k = 0; k < 9; k++) {
-            console.log(numbers[i][k], colNums[k]);
-            if (!isUnique(colNums[k], colNums)) {
-                // console.log(colNums[k], 'is unique in a column');
-                // const temp = numbers[i][k];
-                // numbers[i][k] = numbers[i][k + 1];
-                // numbers[i][k + 1] = temp;
+        const sqNumbers = [];
+
+        for (let m = 0; m < 3; m++) {
+            for (let k = 0; k < 3; k++) {
+                const sqNums = [];
+                for (let i = 0; i < 3; i++) {
+                    for (let j = 0; j < 3; j++) {
+                        sqNums.push(numbers[i + m * 3][j + k * 3]);
+                    }
+                }
+                sqNumbers.push(sqNums);
+            }
+        }
+
+        for (let i = 0; i < 9; i++) {
+            if (!isArrLegit(colNumbers[i])) break;
+            if (!isArrLegit(sqNumbers[i])) break;
+            if (i === 8) {
+                ok = true;
+                console.log(numbers);
             }
         }
     }
@@ -42,7 +60,7 @@ const Sudoku = () => {
     return (
         <div className={s.Sudoku}>
             <div className={s.header}>{isOpenInfo && <Timer seconds={60} func={setTimes} />}</div>
-            <div className={s.problems}>{JSON.stringify(numbers)}</div>
+            <div className={s.problems}>{JSON.stringify('')}</div>
             <InfoBox onClose={closeInfo} isOpen={isOpenInfo} center>
                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis possimus illum
                 aliquam ducimus, iusto praesentium corporis fugit veniam cupiditate minus eveniet,
